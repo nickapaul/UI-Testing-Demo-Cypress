c6878953d8dd3d7120919c86ff1ee90eebfd4ab0
Nicolas Paul <nicolas.paul@outlook.com>
2024-07-09 20:26:16 -0400

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "junit",
+
+  reporterOptions: {
+    mochaFile: "cypress/reports/mocha",
+    toConsole: true,
+  },
+
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+
   e2e: {
     setupNodeEvents(on, config) {
-      // Use the `on` function to hook into various events Cypress emits
-      // e.g., to configure the mochawesome reporter
-      const mochaReporterOptions = {
-        reporter: 'mochawesome',
-        reporterOptions: {
-          reportDir: 'cypress/reports/mocha',
-          overwrite: true,
-          html: false,          
-          xml: true
-        }
-      };
-      on('before:run', () => {
-        console.log('Configuring mochawesome reporter');
-      });
-      // Here you can modify the config object as needed
-      config.reporter = mochaReporterOptions.reporter;
-      config.reporterOptions = mochaReporterOptions.reporterOptions;
-      return config; // Important: return the modified config
+      // implement node event listeners here
     },
-    viewportWidth: 1920,
-    viewportHeight: 1080
   },
 });
